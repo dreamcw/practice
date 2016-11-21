6ed28056fb1bb36bc408c990ae027b26b72125de
changwan <dream_cw@naver.com>
2016-11-21 20:52:31 +0900

    day2

---
+++ day2/day2/day2.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/*void main()
+{
+printf("Demical:%d\n",173);
+printf("Demical:%d\n",0255);
+printf("Demical:%d\n",0xAD);
+
+printf("Octal:%o\n",173);
+printf("Octal:%o\n",0255);
+printf("Octal:%o\n",0xAD);
+
+printf("Hexadecimal:%x\n", 173);
+printf("Hexadecimal:%x\n",0255);
+printf("Hexadecimal:%x\n",0xAD);
+}*/
+
+/*void main()
+
+{
+printf("실수형 상수 : %f \n", 123.456);
+printf("지수형 상수 : %d \n", 123.456);
+printf("실수형 상수 : %f\n", 123);
+}*/
+
+
+/*void main()
+{
+printf("문자 %c의 ASCII 코드 값은 %d 이다.\n", 'a', 'a');
+printf("ASCII 코드 값이 %d인 문자는 %c 이다.\n", 98, 98);
+}*/
+
+void main()
+{
+	printf("======================================================\n");
+	printf("%s", "\t이름\t\t나이\t\t주소\n");
+	printf("======================================================\n");
+	printf("\t홍길동\t\t%d\t\t산골짜기\n", 20);
+	printf("\t%s\t\t%s\t\t%s\n", "이순신", "30", "바다");
+
+}
