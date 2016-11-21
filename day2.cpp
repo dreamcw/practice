5917a8a9a3f86a4f326a4b5343a9394800dbc129
changwan <dream_cw@naver.com>
2016-11-21 20:20:47 +0900

    first commit

---
+++ day2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+	printf("Demical:%d\n",173);
+	printf("Demical:%d\n",0255);
+	printf("Demical:%d\n",0xAD);
+
+	printf("Octal:%o\n",173);
+	printf("Octal:%o\n",0255);
+	printf("Octal:%o\n",0xAD);
+
+	printf("Hexadecimal:%x\n", 173);
+	printf("Hexadecimal:%x\n",0255);
+	printf("Hexadecimal:%x\n",0xAD);
+}
